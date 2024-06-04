0385e91008fbc9079b296f0cacef88a91955a9c0
KboyVillahermosa <villahermosafrancisco6@gmail.com>
2024-06-04 19:41:22 +0800

    final

--- bpms/index.php
+++ bpms/index.php
@@ -3,6 +3,17 @@ include ('includes/dbconnection.php');
 session_start();
 error_reporting(0);
 
+// Function to fetch services from database
+function getServices($con)
+{
+	$services = [];
+	$query = mysqli_query($con, "SELECT * FROM tblservices");
+	while ($row = mysqli_fetch_array($query)) {
+		$services[] = $row['ServiceName'];
+	}
+	return $services;
+}
+
 if (isset($_POST['submit'])) {
 	$name = $_POST['name'];
 	$email = $_POST['email'];
@@ -13,9 +24,14 @@ if (isset($_POST['submit'])) {
 	$phone = $_POST['phone'];
 	$aptnumber = mt_rand(100000000, 999999999);
 
-	$query = mysqli_query($con, "insert into tblappointment(AptNumber,Name,Email,PhoneNumber,AptDate,AptTime,Services) value('$aptnumber','$name','$email','$phone','$adate','$atime','$services')");
+	if ($services === 'Other') {
+		$services = $_POST['other_service'];
+	}
+
+	$query = mysqli_query($con, "INSERT INTO tblappointment(AptNumber,Name,Email,PhoneNumber,AptDate,AptTime,Services) 
+                                VALUES ('$aptnumber','$name','$email','$phone','$adate','$atime','$services')");
 	if ($query) {
-		$ret = mysqli_query($con, "select AptNumber from tblappointment where Email='$email' and  PhoneNumber='$phone'");
+		$ret = mysqli_query($con, "SELECT AptNumber FROM tblappointment WHERE Email='$email' AND PhoneNumber='$phone'");
 		$result = mysqli_fetch_array($ret);
 		$_SESSION['aptno'] = $result['AptNumber'];
 		echo "<script>window.location.href='thank-you.php'</script>";
@@ -28,12 +44,8 @@ if (isset($_POST['submit'])) {
 $appointments_query = mysqli_query($con, "SELECT * FROM tblappointment");
 $appointments = mysqli_fetch_all($appointments_query, MYSQLI_ASSOC);
 
-
-// Fetch existing appointments for display
-$appointments_query = mysqli_query($con, "SELECT * FROM tblappointment");
-$appointments = mysqli_fetch_all($appointments_query, MYSQLI_ASSOC);
+// Group appointments by date and time
 $groupedAppointments = [];
-
 foreach ($appointments as $appointment) {
 	$key = $appointment['AptDate'] . '_' . $appointment['AptTime'];
 	if (!isset($groupedAppointments[$key])) {
@@ -45,7 +57,11 @@ foreach ($appointments as $appointment) {
 	}
 	$groupedAppointments[$key]['count']++;
 }
+
+// Fetch services from database
+$servicesList = getServices($con);
 ?>
+
 
 
 <!DOCTYPE html>
@@ -142,59 +158,31 @@ foreach ($appointments as $appointment) {
 			background-color: #dc3545 !important;
 		}
 
-		.modal-btn {
-			margin-top: 20px;
-			padding: 10px 20px;
-			border: none;
-			background-color: #dc3545;
-			color: #fff;
-			font-size: 16px;
-			cursor: pointer;
-			border-radius: 5px;
-			transition: background-color 0.3s;
-		}
-
-
 		.modal {
 			display: none;
 			position: fixed;
-			z-index: 1;
+			z-index: 999;
 			left: 0;
 			top: 0;
 			width: 100%;
-			height: 100;
-			overflow: auto;
-			background-color: rgba(0, 0, 0, 0.4);
+			height: 100%;
+			overflow-y: auto;
+			background-color: rgba(0, 0, 0, 0.5);
 		}
 
-		/* Modal Content/Box */
 		.modal-content {
 			background-color: #fefefe;
 			margin: 15% auto;
 			padding: 20px;
 			border: 1px solid #888;
 			width: 80%;
-			max-width: 600px;
 		}
 
-		/* Close Button */
 		.close {
 			color: #aaa;
 			float: right;
 			font-size: 28px;
 			font-weight: bold;
-		}
-
-		.close-btn {
-			margin-top: 20px;
-			padding: 10px 20px;
-			border: none;
-			background-color: #dc3545;
-			color: #fff;
-			font-size: 16px;
-			cursor: pointer;
-			border-radius: 5px;
-			transition: background-color 0.3s;
 		}
 
 		.close:hover,
@@ -291,20 +279,24 @@ foreach ($appointments as $appointment) {
 											<div class="select-wrap">
 												<div class="icon"><span class="ion-ios-arrow-down"></span></div>
 												<select name="services" id="services" required="true"
-													class="form-control">
+													class="form-control" onchange="toggleOtherInput()">
 													<option value="">Select Services</option>
-													<?php $query = mysqli_query($con, "select * from tblservices");
-													while ($row = mysqli_fetch_array($query)) {
-														?>
-
-														<option value="<?php echo $row['ServiceName']; ?>">
-															<?php echo $row['ServiceName']; ?>
+													<?php foreach ($servicesList as $service) { ?>
+														<option value="<?php echo $service; ?>"><?php echo $service; ?>
 														</option>
 													<?php } ?>
+													<option value="Other">Other (Please specify)</option>
 												</select>
 											</div>
 										</div>
 									</div>
+									<div class="col-sm-12" id="other-service-div" style="display: none;">
+										<div class="form-group">
+											<input type="text" class="form-control" name="other_service"
+												id="other_service" placeholder="Please specify the service">
+										</div>
+									</div>
+
 									<div class="col-sm-12">
 										<div class="form-group">
 											<div class="select-wrap">
@@ -313,33 +305,14 @@ foreach ($appointments as $appointment) {
 													class="form-control">
 													<option value="">Select Branch</option>
 													<?php $query = mysqli_query($con, "select * from tblbranches");
-													while ($row = mysqli_fetch_array($query)) {
-														?>
+													while ($row = mysqli_fetch_array($query)) { ?>
 														<option value="<?php echo $row['Branch']; ?>">
-															<?php echo $row['Branch']; ?>
-														</option>
+															<?php echo $row['Branch']; ?></option>
 													<?php } ?>
 												</select>
 											</div>
 										</div>
 									</div>
-									<div class="col-sm-12">
-										<div class="form-group">
-											<div class="select-wrap">
-												<div class="icon"><span class="ion-ios-arrow-down"></span></div>
-												<select name="no-of-heads id=" noheads" required="true"
-													class="form-control">
-													<option value="">No. of heads</option>
-													<option value="">1</option>
-													<option value="">2</option>
-													<option value="">3</option>
-													<option value="">4</option>
-												</select>
-											</div>
-										</div>
-									</div>
-
-
 									<div class="col-sm-12">
 										<div class="form-group">
 											<input type="text" class="form-control appointment_date" placeholder="Date"
@@ -348,10 +321,27 @@ foreach ($appointments as $appointment) {
 									</div>
 									<div class="col-sm-12">
 										<div class="form-group">
-											<input type="text" class="form-control appointment_time" placeholder="Time"
-												name="atime" id='atime' required="true">
+											<select class="form-control" name="atime" id="atime" required>
+												<option value="10:30">10:30 AM</option>
+												<option value="11:00">11:00 AM</option>
+												<option value="11:30">11:30 AM</option>
+												<option value="12:00">12:00 PM</option>
+												<option value="12:30">12:30 PM</option>
+												<option value="13:00">1:00 PM</option>
+												<option value="13:30">1:30 PM</option>
+												<option value="14:00">2:00 PM</option>
+												<option value="14:30">2:30 PM</option>
+												<option value="15:00">3:00 PM</option>
+												<option value="15:30">3:30 PM</option>
+												<option value="16:00">4:00 PM</option>
+												<option value="16:30">4:30 PM</option>
+												<option value="17:00">5:00 PM</option>
+												<option value="17:30">5:30 PM</option>
+												<option value="18:00">6:00 PM</option>
+											</select>
 										</div>
 									</div>
+
 									<div class="col-sm-12">
 										<div class="form-group">
 											<input type="text" class="form-control" id="phone" name="phone"
@@ -359,21 +349,25 @@ foreach ($appointments as $appointment) {
 										</div>
 									</div>
 								</div>
-								<div class="form-group">
+								<div class="form-group" style="display: flex; gap: 10px;">
+									<button type="submit" name="submit" value="" class="show btn btn-primary"
+										onclick="openModal()">SHOW
+										APPOINTMENT</button>
 									<input type="submit" name="submit" value="Make an Appointment"
 										class="btn btn-primary">
 								</div>
 								<span class="subheading">"Vi Vogue Online Salon Reservation is committed to protecting
-									the privacy of our customers. <br>
-									We collect and use your personal information in accordance with Republic Act No.
-									10173 or the <br>
-									Data Privacy Act of 2012 and our privacy policy. By using our website to book
-									appointments, <br>
-									you consent to the collection and use of your personal information in accordance
-									with this policy.<br>
-									If you have any questions or concerns regarding our data privacy practices, please
-									contact us."</span>
+									the privacy of our
+									customers. <br> We collect and use your personal information in accordance with
+									Republic Act No. 10173 or
+									the <br> Data Privacy Act of 2012 and our privacy policy. By using our website to
+									book appointments, <br> you
+									consent to the collection and use of your personal information in accordance with
+									this policy.<br> If you have
+									any questions or concerns regarding our data privacy practices, please contact
+									us."</span>
 							</form>
+
 						</div>
 					</div>
 				</div>
@@ -388,128 +382,82 @@ foreach ($appointments as $appointment) {
 					</div>
 				</div>
 			</div>
-
-
 	</section>
 
-	<!-- Appointment History Section -->
-	<section class="ftco-section bg-light">
-		<div class="container">
-			<div class="row justify-content-center mb-5 pb-3">
-				<div class="col-md-7 heading-section text-center ftco-animate">
-					<span class="subheading">Appointment History</span>
-					<h2 class="mb-4">Your Appointments</h2>
+	<div id="myModal" class="modal">
+		<div class="modal-content">
+			<span class="close" onclick="closeModal()">&times;</span>
+			<div class="container">
+				<div class="row justify-content-center mb-5 pb-3">
+					<div class="col-md-7 heading-section text-center ftco-animate">
+						<span class="subheading">Appointment History</span>
+					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-12 ftco-animate">
-					<div class="appointment-history">
-						<h3>Current Appointments</h3>
-						<ul class="appointments-list">
-							<?php
-							// Array to store aggregated appointments
-							$aggregatedAppointments = [];
+				<div class="row">
+					<div class="col-md-12 ftco-animate">
+						<div class="appointment-history">
+							<h3>Current Appointments</h3>
+							<ul class="appointments-list">
+								<?php
+								// Array to store aggregated appointments
+								$aggregatedAppointments = [];
 
-							// Group appointments by date and time
-							foreach ($appointments as $appointment) {
-								$key = $appointment['AptDate'] . '_' . $appointment['AptTime'];
-								if (!isset($aggregatedAppointments[$key])) {
-									$aggregatedAppointments[$key] = [
-										'AptDate' => $appointment['AptDate'],
-										'AptTime' => $appointment['AptTime'],
-										'Count' => 0
-									];
+								// Group appointments by date and time
+								foreach ($appointments as $appointment) {
+									$key = $appointment['AptDate'] . '_' . $appointment['AptTime'];
+									if (!isset($aggregatedAppointments[$key])) {
+										$aggregatedAppointments[$key] = [
+											'AptDate' => $appointment['AptDate'],
+											'AptTime' => $appointment['AptTime'],
+											'Count' => 0
+										];
+									}
+									$aggregatedAppointments[$key]['Count']++;
 								}
-								$aggregatedAppointments[$key]['Count']++;
-							}
 
-							// Display aggregated appointments
-							foreach ($aggregatedAppointments as $appointment) {
-								?>
-								<li>
-									<div class="appointment-details">
-										<div class="appointment-meta">
-											<span
-												class="date"><?php echo date('F j, Y', strtotime($appointment['AptDate'])); ?></span>
-											<span
-												class="time"><?php echo date('h:i A', strtotime($appointment['AptTime'])); ?></span>
-										</div>
-										<div class="progress-bar-container">
-											<?php
-											// Calculate progress
-											$progress = ($appointment['Count'] / 10) * 100; // Number of slots per schedule
-											$remainingSlots = 10 - $appointment['Count'];
-											?>
-											<div class="progress">
-												<div class="progress-bar <?php echo ($progress >= 100) ? 'bg-danger' : 'bg-success'; ?>"
-													role="progressbar" style="width: <?php echo $progress; ?>%;"
-													aria-valuenow="<?php echo $progress; ?>" aria-valuemin="0"
-													aria-valuemax="100"></div>
+								// Display aggregated appointments
+								foreach ($aggregatedAppointments as $appointment) {
+									?>
+									<li>
+										<div class="appointment-details">
+											<div class="appointment-meta">
+												<span
+													class="date"><?php echo date('F j, Y', strtotime($appointment['AptDate'])); ?></span>
+												<span
+													class="time"><?php echo date('h:i A', strtotime($appointment['AptTime'])); ?></span>
 											</div>
-											<div class="slots-left">
-												Slots Left: <?php echo $remainingSlots; ?>
+											<div class="progress-bar-container">
+												<?php
+												// Calculate progress
+												$progress = ($appointment['Count'] / 10) * 100; // Number of slots per schedule
+												$remainingSlots = 10 - $appointment['Count'];
+												?>
+												<div class="progress">
+													<div class="progress-bar <?php echo ($progress >= 100) ? 'bg-danger' : 'bg-success'; ?>"
+														role="progressbar" style="width: <?php echo $progress; ?>%;"
+														aria-valuenow="<?php echo $progress; ?>" aria-valuemin="0"
+														aria-valuemax="100"></div>
+												</div>
+												<div class="slots-left">
+													Slots Left: <?php echo $remainingSlots; ?>
+												</div>
 											</div>
 										</div>
-									</div>
-								</li>
-							<?php } ?>
-						</ul>
-
-						<button id="openModalBtn" class="modal-btn">More Appointment</button>
-
-						<div id="myModal" class="modal">
-							<div class="modal-content">
-								<span class="close">&times;</span>
-								<?php if (count($aggregatedAppointments) > 5) { ?>
-									<div class="more-appointments hidden">
-										<h3>More Appointments</h3>
-										<ul class="appointments-list">
-											<?php foreach ($aggregatedAppointments as $key => $appointment) {
-												if ($key >= 5) { ?>
-													<li>
-														<div class="appointment-details">
-															<div class="appointment-meta">
-																<span
-																	class="date"><?php echo date('F j, Y', strtotime($appointment['AptDate'])); ?></span>
-																<span
-																	class="time"><?php echo date('h:i A', strtotime($appointment['AptTime'])); ?></span>
-															</div>
-															<div class="progress-bar-container">
-																<?php
-																// Calculate progress
-																$progress = ($appointment['Count'] / 10) * 100; // Number of slots per schedule
-																$remainingSlots = 10 - $appointment['Count'];
-																?>
-																<div class="progress">
-																	<div class="progress-bar <?php echo ($progress >= 100) ? 'bg-danger' : 'bg-success'; ?>"
-																		role="progressbar" style="width: <?php echo $progress; ?>%;"
-																		aria-valuenow="<?php echo $progress; ?>" aria-valuemin="0"
-																		aria-valuemax="100"></div>
-																</div>
-																<div class="slots-left">
-																	Slots Left: <?php echo $remainingSlots; ?>
-																</div>
-															</div>
-														</div>
-													</li>
-												<?php }
-											} ?>
-										</ul>
-									</div>
-
-
+									</li>
 								<?php } ?>
-								<button id="closeModalBtn" class="close-btn">Close</button>
-							</div>
+							</ul>
+							<button onclick="closeModal()" class="btn btn-primary">Close Modal</button>
 						</div>
-
-
-
 					</div>
 				</div>
 			</div>
 		</div>
-	</section>
+	</div>
+	</div>
+	</div>
+
+
+
 
 	<!-- Loader -->
 	<?php include_once ('includes/footer.php'); ?>
@@ -560,35 +508,30 @@ foreach ($appointments as $appointment) {
 			});
 
 		});
-		// Get the modal
 		var modal = document.getElementById("myModal");
 
 		// Get the button that opens the modal
-		var btn = document.getElementById("openModalBtn");
+		var btn = document.querySelector("button");
 
-		// Get the <span> element that closes the modal
-		var span = document.getElementsByClassName("close")[0];
-
-		// When the user clicks on the button, open the modal
-		btn.onclick = function () {
+		// When the user clicks the button, open the modal 
+		function openModal() {
 			modal.style.display = "block";
 		}
 
 		// When the user clicks on <span> (x), close the modal
-		span.onclick = function () {
+		function closeModal() {
 			modal.style.display = "none";
 		}
-
-		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function (event) {
-			if (event.target == modal) {
-				modal.style.display = "none";
+		function toggleOtherInput() {
+			var select = document.getElementById('services');
+			var otherServiceDiv = document.getElementById('other-service-div');
+			if (select.value === 'Other') {
+				otherServiceDiv.style.display = 'block';
+				document.getElementById('other_service').required = true;
+			} else {
+				otherServiceDiv.style.display = 'none';
+				document.getElementById('other_service').required = false;
 			}
-		}
-
-		// Close modal on button click inside modal
-		document.getElementById("closeModalBtn").onclick = function () {
-			modal.style.display = "none";
 		}
 
 	</script>
